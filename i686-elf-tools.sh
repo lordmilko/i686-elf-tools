9443051873d02c273e09b5a929c35262503ec0c4
Samuel <40303843+sasdallas@users.noreply.github.com>
2024-11-13 18:34:43 -0600

    Use cat method and apt-cache method

--- i686-elf-tools.sh
+++ i686-elf-tools.sh
@@ -138,9 +138,8 @@ function installPackages {
         lzip)
     echoColor "Installing packages"
 
-    # Fix correct python packages on modern Ubuntu versions
-    source /etc/os-release || true # Zorin OS doesn't use lsb_release, but rather uses $ID_LIKE
-    if [[ $(lsb_release -a) =~ .*"Ubuntu".*$ || $ID_LIKE =~ "ubuntu" ]]; then
+    # Fix correct python packages on modern Ubuntu and Ubuntu-based distros
+    if [[ $(lsb_release -a) =~ .*"Ubuntu".*$ || $(cat /etc/os-release) =~ .*(U|u)buntu.*$ || $(apt-cache search --names-only ^python$ | wc -m) -gt 0 ]]; then
         pkgList+=(python3 python-is-python3)
     else
         pkgList+=(python)
